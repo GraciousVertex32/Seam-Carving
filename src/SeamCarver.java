49c821d10995aa6edca55115d8dba12846861c8a
GraciousVertex32 <31588270+GraciousVertex32@users.noreply.github.com>
2018-06-12 18:41:13 +0800

    Final
    80/100

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,16 +1,15 @@
 import edu.princeton.cs.algs4.Picture;
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.List;
-import java.util.Collections;
+import java.awt.Color;
 
 public class SeamCarver
 {
     private Picture picture;
     private int[][] rgbarray;
     private double[][] energy;
+
     public SeamCarver(Picture picture)                // create a seam carver object based on the given picture
     {
         this.picture = picture;
@@ -20,98 +19,164 @@ public class SeamCarver
         {
             for (int j = 0; j < picture.height(); j++)
             {
-                rgbarray[i][j] = picture.getRGB(i,j);
+                rgbarray[i][j] = picture.getRGB(i, j);
             }
         }
         for (int i = 0; i < picture.width(); i++)
         {
             for (int j = 0; j < picture.height(); j++)
             {
-                energy[i][j] = caculateenergy(i,j);
+                energy[i][j] = calculateenergy(i, j, rgbarray);
             }
         }
     }
+
     public Picture picture()                          // current picture
     {
         return picture;
     }
-    public     int width()                            // width of current picture
+
+    public int width()                            // width of current picture
     {
         return picture.width();
     }
-    public     int height()                           // height of current picture
+
+    public int height()                           // height of current picture
     {
-        return  picture.height();
+        return picture.height();
     }
-    public  double energy(int x, int y)               // energy of pixel at column x and row y
+
+    public double energy(int x, int y)               // energy of pixel at column x and row y
     {
+        if (x < 0 || y < 0 || x >= energy.length || y >= energy[0].length)
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
         return energy[x][y];
     }
-    public   int[] findHorizontalSeam()               // sequence of indices for horizontal seam
+
+    public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
     {
         return ShortestPath(energy);
     }
-    public   int[] findVerticalSeam()                 // sequence of indices for vertical seam
+
+    public int[] findVerticalSeam()                 // sequence of indices for vertical seam
     {
         int width = energy.length;
         int height = energy[0].length;
         double[][] array_new = new double[height][width];
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
                 array_new[y][x] = energy[x][y];
             }
         }
         return ShortestPath(array_new);
     }
-    public    void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
-    {
 
-    }
-    public    void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
+    public void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
     {
+        if (seam == null || picture.height() == 1 || seam.length != picture.width())
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+        rgbarray = newrgb(rgbarray,seam);
+        energy = newenergy(energy,seam,rgbarray);
+        Picture p2 = new Picture(picture.width(), picture.height() - 1);
+        for (int i = 0; i < p2.width(); i++)
+        {
+            for (int j = 0; j < p2.height(); j++)
+            {
+                p2.set(i, j, rgbtocolor(rgbarray[i][j]));
+            }
+        }
+        picture = p2;
+    }
 
-    }
-    private double caculateenergy(int x, int y)
+    public void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
     {
-        if (x == 0 || x == picture.width() - 1 || y == 0 || y == picture.height() -1 )
+        if (seam == null || picture.width() == 1 || seam.length != picture.height())
+        {
+            throw new java.lang.IllegalArgumentException();
+        }
+        int[][] TransposedRgb = new int[rgbarray[0].length][rgbarray.length];
+        for (int i = 0; i < rgbarray[0].length; i++)
+        {
+            for (int j = 0; j < rgbarray.length; j++)
+            {
+                TransposedRgb[i][j] = rgbarray[j][i];
+            }
+        }
+        int[][] TempRgb = newrgb(TransposedRgb,seam);
+        double[][] TransposedEnergy = new double[energy[0].length][energy.length];
+        for (int i = 0; i < energy[0].length; i++)
+        {
+            for (int j = 0; j < energy.length; j++)
+            {
+                TransposedEnergy[i][j] = energy[j][i];
+            }
+        }
+        double[][] TempEnergy = newenergy(TransposedEnergy,seam,TempRgb);
+        Picture TransposedP = new Picture(picture.width() - 1, picture.height());
+        for (int i = 0; i < TransposedP.width(); i++)
+        {
+            for (int j = 0; j < TransposedP.height(); j++)
+            {
+                TransposedP.set(i, j, rgbtocolor(TempRgb[j][i]));
+            }
+        }
+        rgbarray = new int[rgbarray.length-1][rgbarray[0].length];
+        energy = new double[energy.length-1][energy[0].length];
+        for (int i = 0; i < rgbarray.length; i++)
+        {
+            for (int j = 0; j < rgbarray[0].length; j++)
+            {
+                rgbarray[i][j] = TempRgb[j][i];
+                energy[i][j] = TempEnergy[j][i];
+            }
+        }
+        picture = TransposedP;
+    }
+
+    private double calculateenergy(int x, int y,int[][] rgbarray) // calculate
+    {
+        if (x == 0 || x == rgbarray.length - 1 || y == 0 || y == rgbarray[0].length - 1)
         {
             return 1000;
         }
-        if (x == 10 && y == 10)
-        {
-            int a = 1;
-        }
-        int rgbl = rgbarray[x-1][y];
+        int rgbl = rgbarray[x - 1][y];
         int rl = (rgbl >> 16) & 0xFF;
-        int gl = (rgbl >>  8) & 0xFF;
-        int bl = (rgbl >>  0) & 0xFF;
+        int gl = (rgbl >> 8) & 0xFF;
+        int bl = (rgbl >> 0) & 0xFF;
 
-        int rgbr = rgbarray[x+1][y];
+        int rgbr = rgbarray[x + 1][y];
         int rr = (rgbr >> 16) & 0xFF;
-        int gr = (rgbr >>  8) & 0xFF;
-        int br = (rgbr >>  0) & 0xFF;
-        
-        int rgbu = rgbarray[x][y-1];
+        int gr = (rgbr >> 8) & 0xFF;
+        int br = (rgbr >> 0) & 0xFF;
+
+        int rgbu = rgbarray[x][y - 1];
         int ru = (rgbu >> 16) & 0xFF;
-        int gu = (rgbu >>  8) & 0xFF;
-        int bu = (rgbu >>  0) & 0xFF;
+        int gu = (rgbu >> 8) & 0xFF;
+        int bu = (rgbu >> 0) & 0xFF;
 
-        int rgbd = rgbarray[x][y+1];
+        int rgbd = rgbarray[x][y + 1];
         int rd = (rgbd >> 16) & 0xFF;
-        int gd = (rgbd >>  8) & 0xFF;
-        int bd = (rgbd >>  0) & 0xFF;
+        int gd = (rgbd >> 8) & 0xFF;
+        int bd = (rgbd >> 0) & 0xFF;
 
-        double yieldingx = Math.pow(rl-rr,2) + Math.pow(gl-gr,2) + Math.pow(bl-br,2);
-        double yieldingy = Math.pow(ru-rd,2) + Math.pow(gu-gd,2) + Math.pow(bu-bd,2);
-        return Math.pow(yieldingx + yieldingy,0.5);
+        double yieldingx = Math.pow(rl - rr, 2) + Math.pow(gl - gr, 2) + Math.pow(bl - br, 2);
+        double yieldingy = Math.pow(ru - rd, 2) + Math.pow(gu - gd, 2) + Math.pow(bu - bd, 2);
+        return Math.pow(yieldingx + yieldingy, 0.5);
     }
-    private int[] ShortestPath(double[][] array) // shortest vertical path
+
+    private int[] ShortestPath(double[][] array) // shortest horizontal path
     {
         double[][] energy;
         Map<Integer, Integer> EdgeTo = new HashMap<Integer, Integer>();
         Map<Integer, Double> DistTo = new HashMap<Integer, Double>();
-        energy = Arrays.copyOf(array,array.length);
+        energy = Arrays.copyOf(array, array.length);
         int width = energy.length;
         int height = energy[0].length;
         double shortest = Double.MAX_VALUE;
@@ -120,29 +185,25 @@ public class SeamCarver
         {
             for (int j = 0; j < height; j++)
             {
-                if (i == 8 && j == 5)
-                {
-                    int a = 1;
-                }
                 if (i == 0)// first Vline
                 {
                     DistTo.put(mip(i, j), energy[i][j]);
-                    EdgeTo.put(mip(i,j),null);
+                    EdgeTo.put(mip(i, j), null);
                 }
-                for (int k = -1; k <= 1; k++ )// each 3 following pixel
+                for (int k = -1; k <= 1; k++)// each 3 following pixel
                 {
                     if ((j + k) >= 0 && (j + k) < height) // is within boundary
                     {
                         double currentdist = DistTo.get(mip(i, j));
                         double newdist = currentdist + energy[i + 1][j + k];
-                        int nextpixel = mip(i+1, j + k);
+                        int nextpixel = mip(i + 1, j + k);
                         // not calculated yet or shorter than existing
                         if (DistTo.get(nextpixel) == null || DistTo.get(nextpixel) > newdist)
                         {
                             EdgeTo.put(nextpixel, mip(i, j));
                             DistTo.put(nextpixel, newdist);
                         }
-                        if ( i == width - 2 && newdist < shortest)
+                        if (i == width - 2 && newdist < shortest)
                         {
                             shortest = newdist;
                             last = nextpixel;
@@ -166,21 +227,66 @@ public class SeamCarver
         int[] end = new int[width];
         for (int i = 0; i < mapped.length; i++)
         {
-            end[end.length-1-i] = mapped[i];
+            end[end.length - 1 - i] = mapped[i];
         }
         return end;
     }
 
     private int mip(int x, int y) // map index pair
     {
-        if (x < 65535 && y <65535)
+        if (x < 65535 && y < 65535)
         {
             return (x << 16) + y;
         }
         throw new IllegalArgumentException("too big");
     }
-    private String mip2(Integer x, Integer y)
+
+    private int[][] newrgb(int[][] original, int[] seam) // change rgb array after seam
     {
-        return x.toString()+","+y.toString();
+        int[][] newarray = new int[original.length][original[0].length - 1];
+        for (int x = 0; x < newarray.length; x++)
+        {
+            for (int y = 0; y < newarray[0].length; y++)
+            {
+                if (y < seam[x])
+                {
+                    newarray[x][y] = original[x][y];
+                } else
+                {
+                    newarray[x][y] = original[x][y + 1];
+                }
+            }
+        }
+        return newarray;
+    }
+
+    private double[][] newenergy(double[][] origin, int[] seam,int[][] updatedarray) // change energy array after seam
+    {
+        double[][] newarray = new double[origin.length][origin[0].length - 1];
+        for (int x = 0; x < newarray.length; x++)
+        {
+            for (int y = 0; y < newarray[0].length; y++)
+            {
+                if (y < seam[x] - 1)
+                {
+                    newarray[x][y] = origin[x][y];
+                } else if (y > seam[x])
+                {
+                    newarray[x][y] = origin[x][y + 1];
+                }
+                else
+                {
+                    newarray[x][y] = calculateenergy(x,y,updatedarray);
+                }
+            }
+        }
+        return newarray;
+    }
+    private Color rgbtocolor(int rgb)
+    {
+        int r = rgb >> 16 & 0xFF;
+        int g = rgb >> 8 & 0xFF;
+        int b = rgb >> 0 & 0xFF;
+        return new Color(r,g,b);
     }
 }
